a7993db8987fc954f5a16b844933c05376463940
fpumir <fpumir@terredeweb.fr>
2013-10-24 16:56:42 +0200

    video js poo
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

---
+++ js/video_oo.js
@@ -0,0 +1,46 @@
+var player= function (params){
+	var self=this;
+	self.video=document.querySelector(params.media);
+	self.controller=document.querySelector(params.controller);
+	self.progress=document.querySelector(params.progress);
+	self.buffer=document.querySelector(params.buffer);
+	
+	self.video.load();
+	
+	self.playpause=function(){ 
+		
+		if(self.video.paused){
+			self.video.play();
+			params.played.call();
+			
+		}else{
+			self.video.pause();
+			params.paused.call();
+		}
+	}
+	
+	self.setVideoTime=function(e){ 
+		var x=e.offsetX;
+		var newTime=x*self.video.duration/this.offsetWidth;
+		self.video.currentTime=newTime;
+		params.moved.call();
+	}
+	
+	
+	self.updateProgress = function (){
+		var progressWidth=this.currentTime*100/this.duration;
+		self.progress.style.width=progressWidth+'%';
+
+		var bufferWidth=this.buffered.end(0)*100/this.duration;
+		self.buffer.style.width=bufferWidth+'%';
+		
+	}
+	
+	//si canplaythrough
+	self.video.addEventListener('canplaythrough',function(){
+		//on appelle le callback loaded en passant self en argument.
+		params.loaded.call(this,self);
+	},false);
+	self.video.addEventListener('timeupdate',self.updateProgress,false);
+
+};
